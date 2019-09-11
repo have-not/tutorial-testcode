3ba37e239fa98f42e926ea5e70f9eb48a9e01e32
matsuoka hiromu <matsuokahiromu@matsuokanoMacBook-Air.local>
2019-09-11 14:11:19 +0900

    fix hw

--- helloworld/test_programming.py
+++ helloworld/test_programming.py
@@ -1,12 +1,10 @@
 import unittest
 import programming as pg
 
-
 class TestHelloWorld(unittest.TestCase):
 
     def test_helloworld(self):
         self.assertEqual(pg.helloworld(), 'HelloWorld!')
-
 
 if __name__ == '__main__':
     unittest.main()
